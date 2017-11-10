4df5c008b8c67f0e308676f86ee29439b95d815e
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2017-11-10 23:03:34 +0100

    Changed tabs to spaces.

--- Like.java
+++ Like.java
@@ -1,45 +1,45 @@
 public class Like {
 
-	/**
-	 * Visual Basic's Like operator in Java.<br />
-	 * <br />
-	 * <code>?</code> matches exactly one character<br />
-	 * <code>*</code> matches zero or more characters<br />
-	 * <code>#</code> matches exactly one digit<br />
-	 * 
-	 * @author Urs Schmidt
-	 */
-	public static boolean like(String str, String pattern) {
-		final int sl = str.length();
-		final int pl = pattern.length();
-		for (int i = 0; i < pl; i++) {
-			final char pc = pattern.charAt(i);
-			if (pc != '*' && i == sl)
-				return false;
-			switch (pc) {
-			case '?':
-				/* always matches */
-				break;
-			case '*':
-				if (i + 1 == pl)
-					return true;
-				final String ps = pattern.substring(i + 1);
-				for (int j = i; j < sl; j++) {
-					if (like(str.substring(j), ps))
-						return true;
-				}
-				return false;
-			case '#':
-				if (!Character.isDigit(str.charAt(i)))
-					return false;
-				break;
-			default:
-				if (pc != str.charAt(i))
-					return false;
-			}
-		}
-		return sl == pl;
-	}
+    /**
+     * Visual Basic's Like operator in Java.<br />
+     * <br />
+     * <code>?</code> matches exactly one character<br />
+     * <code>*</code> matches zero or more characters<br />
+     * <code>#</code> matches exactly one digit<br />
+     * 
+     * @author Urs Schmidt
+     */
+    public static boolean like(String str, String pattern) {
+        final int sl = str.length();
+        final int pl = pattern.length();
+        for (int i = 0; i < pl; i++) {
+            final char pc = pattern.charAt(i);
+            if (pc != '*' && i == sl)
+                return false;
+            switch (pc) {
+            case '?':
+                /* always matches */
+                break;
+            case '*':
+                if (i + 1 == pl)
+                    return true;
+                final String ps = pattern.substring(i + 1);
+                for (int j = i; j < sl; j++) {
+                    if (like(str.substring(j), ps))
+                        return true;
+                }
+                return false;
+            case '#':
+                if (!Character.isDigit(str.charAt(i)))
+                    return false;
+                break;
+            default:
+                if (pc != str.charAt(i))
+                    return false;
+            }
+        }
+        return sl == pl;
+    }
 
     public static void main(String[] args) {
         if (args.length < 2) {
